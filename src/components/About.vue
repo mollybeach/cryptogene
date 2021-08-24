257bef95134decf806b72466e53845d8370422e9
mollybeach <mollyjbeach@gmail.com>
2021-08-23 18:13:29 -0700

    intialized vue project

---
+++ src/components/About.vue
@@ -0,0 +1,48 @@
+<template>
+    <main class="main">
+        <div class="about-container">
+            <div class="about-inside">
+                <div class="about-info">
+                    <h1>About</h1>
+                        <strong>
+                            <a href="https://github.com/mollybeach" target="_blank" rel="noopener noreferrer" ><img src="https://avatars0.githubusercontent.com/u/mollybeach" alt="" /><span>@</span> <span>Molly Beach</span> </a>
+                        </strong><br />
+                        <small>
+                            <a href="  " target="_blank" rel="noopener noreferrer">
+                                <i class="fa fa-github"></i>
+                                <span>github</span>
+                            </a>
+                        </small>
+                        <strong>
+                            <a href="" target="_blank" rel="noopener noreferrer"><img src="https://avatars0.githubusercontent.com/u/16097?s=460&v=4" alt="" /><span>@</span> <span>Yuta Moxley</span> </a></strong>
+                        </div>
+                    </div>
+                </div>
+        </main>
+</template>
+
+<script>
+
+export default {
+    data: function () {
+    return {
+        roomName: "About",
+    };
+    },
+    components: {
+    },
+    mounted() {
+        this.$store.commit("sculpturesLoaded", true);
+    },
+    destroyed() {
+    this.$store.commit("leaveRoom", this.roomName);
+    },
+};
+</script>
+
+<style lang="less" scoped>
+    @import "../client/mixins.less";
+    .container {
+    font-size: 1rem;
+    }
+</style>
